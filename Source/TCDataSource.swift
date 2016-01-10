0f9d8f9f26580b710b521b393f21b275da01bc9d
Shaw <cuzval@gmail.com>
2016-01-10 20:53:45 +0800

    no message

--- Source/TCDataSource.swift
+++ Source/TCDataSource.swift
@@ -54,3 +54,35 @@ public class TCDataSource//: UITableViewDataSource, UICollectionViewDataSource
         }
     }
 }
+
+/// So I try extension `TCDataSourceProtocol` implement `UITableViewDataSource`.
+/// Failed agagin.
+public extension TCDataSourceProtocol where Self: UITableViewDataSource {
+    public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var reusable: UITableViewCell
+        if let cell = tableView.dequeueReusableCellWithIdentifier("Cell") {
+            reusable = cell
+        } else {
+            reusable = UITableViewCell(style: .Default, reuseIdentifier: "Cell")
+        }
+        
+        reusable.textLabel?.text = "hhhhhh"
+        
+        
+        return reusable
+    }
+    
+    public func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+}
+
+/// Test case.
+/// I guess I can not forward untill Apple do some improve...
+public class DS: NSObject, TCDataSourceProtocol, UITableViewDataSource {
+    
+}
